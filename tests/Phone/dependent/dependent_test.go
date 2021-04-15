a2845cf7169f0b4f52e46ca3c5d15bf568263a42
😎 Mostafa Emami <mustafaemami@gmail.com>
2021-04-15 07:11:38 +0200

    Create gomod import annotation use --dependency instead of --src argument in generator fix enum clashes make more robust package name

--- tests/Phone/dependent/dependent_test.go
+++ tests/Phone/dependent/dependent_test.go
@@ -2,7 +2,7 @@ package test
 
 import phone "github.com/idleroamer/goqface/tests/Phone/dependent/Tests/Phone"
 
-//go:generate python3 ../../../codegen.py --src ../ --input dependent/Phone.qface
+//go:generate python3 ../../../generator/codegen.py --dependency ../dependency --input Phone.qface
 //go:generate gofmt -w Tests
 
 type PhoneImpl struct {
